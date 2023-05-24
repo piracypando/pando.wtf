eaba662bbccbdb1e0c57c0b5fa1e1ecf7db1955e
piracypando <125031632+piracypando@users.noreply.github.com>
2023-05-24 20:09:22 +0200

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,8 +22,8 @@ class _app {
     "pando#0001",
     "i love my homies",
     "i love photography",
-    "pando#0001",
     "Graphic and media designer",
+    "anton@fbi.systems"
   ];
   titleChanger = (text, delay) => {
     if (!text) return;
@@ -35,30 +35,6 @@ class _app {
     setInterval(() => {
       if (counter < text.length) document.title = text[counter++];
       else document.title = text[(counter = 0)];
-    }, delay);
-  };
-
-  iconChanger = (urls, delay) => {
-    if (!urls) return;
-
-    delay = delay || 2000;
-
-    let counter = 0;
-
-    setInterval(() => {
-      if (counter < urls.length) {
-        const link =
-          document.querySelector("link[rel*='icon']") ||
-          document.createElement("link");
-
-        link.type = "image/x-icon";
-        link.rel = "shortcut icon";
-        link.href = urls[counter];
-
-        document.getElementsByTagName("head")[0].appendChild(link);
-      } else counter = 0;
-
-      ++counter;
     }, delay);
   };
 }
